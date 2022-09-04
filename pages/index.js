90bfd0677862f7a966200a305891ea30868f910a
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-04 23:27:25 +0600

    feat[Tailwind config completed]

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
+export default function Home() {
+  return <div className="bg-red-400 text-center text-3xl">Welcome to Home</div>;
+}
