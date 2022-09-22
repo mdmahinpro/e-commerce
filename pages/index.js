0841a44795bec4f2985baee108368c5422f844c6
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-22 23:12:06 +0600

    refactored

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-
 import Navbar from "../components/general/Navbar";
 import HomePage from "../components/home-page-UI";
-
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-center text-3xl">Welcome to Home</h1>
       <HomePage />
     </div>
   );
